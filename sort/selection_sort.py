d0fb1dae987432f07937e813125b3f8507ff5294
PamelaKelly <pamelakelly7@gmail.com>
2018-02-12 09:11:40 +0000

    change to static method

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -1,28 +1,27 @@
 class SelectionSort:
     
-    def __init__(self, unsorted_list):
-        self.__list = unsorted_list
-        self.__length = len(unsorted_list)
+    def __init__(self):
+        pass
     
-    def sort_ascending(self):
-        for i in range(self.__length):
+    @staticmethod
+    def sort_ascending(lst):
+        for i in range(len(lst)):
             # get the minimum in the rest of the list
-            min_num = min(self.__list[i:])
+            min_num = min(lst[i:])
             
             # get the index of the minimum to do swap later
-            min_index = self.__list.index(min_num)
+            min_index = lst.index(min_num)
             
             # store the value at current index in temp variable
-            temp = self.__list[i]
+            temp = lst[i]
             
             # swap the values
-            self.__list[i] = min_num
-            self.__list[min_index] = temp
+            lst[i] = min_num
+            lst[min_index] = temp
             
-        return self.__list
+        return lst
             
 my_list = [2, 4, 1, 9, 8, 10, 6]
-sorter = SelectionSort(my_list)
-print(sorter.sort_ascending())
+print(SelectionSort.sort_ascending(my_list))
 print(my_list)
         
